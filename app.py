91144ca0c5565e011446f56d8c25d83ec1b1448f
rizviabbas695-ctrl <rizviabbas695@gmail.com>
2026-09-16 17:34:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+import os
+
+st.title("Mera AI Chatbot")
+
+os.environ["GEMINI_API_KEY"] = st.secrets["GEMINI_API_KEY"]
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        SystemMessage(content="You are a helpful assistant.")
+    ]
+
+for message in st.session_state.chat_history:
+    if isinstance(message, HumanMessage):
+        st.chat_message("user").write(message.content)
+    elif isinstance(message, AIMessage):
+        st.chat_message("assistant").write(message.content)
+
+user_input = st.chat_input("Apna sawaal likho...")
+
+if user_input:
+    st.session_state.chat_history.append(HumanMessage(content=user_input))
+    st.chat_message("user").write(user_input)
+
+    response = llm.invoke(st.session_state.chat_history)
+    
+    st.session_state.chat_history.append(AIMessage(content=response.content))
+    st.chat_message("assistant").write(response.content)
